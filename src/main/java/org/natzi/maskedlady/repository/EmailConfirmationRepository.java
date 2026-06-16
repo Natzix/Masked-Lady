6353f0474470eae5a7dc5dfb2c880902304a95b4
Renato Robinson <jhoseppantojarobinson@gmail.com>
2026-06-16 17:03:07 -0500

    New functionality has been added to the scheduled task

--- src/main/java/org/natzi/maskedlady/repository/EmailConfirmationRepository.java
+++ src/main/java/org/natzi/maskedlady/repository/EmailConfirmationRepository.java
@@ -4,11 +4,11 @@ import org.natzi.maskedlady.entity.EmailConfirmationUUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface EmailConfirmationRepository extends JpaRepository<EmailConfirmationUUID, Integer> {
-
+    void deleteByExpiresAtBefore(LocalDateTime date);
     Optional<EmailConfirmationUUID> findByCodConfirmation(String codConfirmation);
 }
